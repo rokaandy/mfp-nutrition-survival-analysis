af114856cc454252f030659af6665f2a4235cfd1
Andy Luc <rokaandy@gmail.com>
2019-07-08 16:37:32 -0700

    Unable to output user input

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, request, render_template, jsonify
+import pandas as pd
+import numpy as np
+import pickle
 
 app = Flask(__name__, static_url_path="")
-
 
 @app.route("/")
 def index():
     """Return the main page."""
     return render_template("index.html")
+
+# with open('pkl/model.pkl', 'rb') as f:
+#     model = pickle.load(f)
+
+# def prediction(user_input, model):
+#     """Predict when a user will drop off the application"""
+#     user_input = np.array((user_input))
+#     df = pd.DataFrame(user_input).T
+#     probability = model.predict_proba(df)
+#     return probability
 
 @app.route("/output", methods=["GET", "POST"])
 def output():
@@ -15,7 +27,7 @@ def output():
     # print the user input data to console
     print(data)
     # store user input data in dictionary
-    output = {"calories": data["calories"],
-              "carbs": data["carbs"]}
+    df = pd.DataFrame(data=[data])
+    print(df)
     # output dictionary to web
-    return jsonify(output)
+    return jsonify(data)
